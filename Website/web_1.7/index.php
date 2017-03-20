3e698274dd1ddf284dabc3e4fd427eddf4935fe9
Kutay Yalçinkaya <kutay_yal@outlook.com>
2017-03-20 14:17:42 +0100

    Webstie version 2
    this is the latest updated website with all the elemants

---
+++ Website/web_1.7/index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya" rel="stylesheet">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="header"><a name="home"></a>
+    <div class="header-space-between">
+        <div class="logo">
+            <h3>Gokkers</h3>
+        </div>
+        <nav>
+            <a href="#home">Home</a>
+            <a href="#regester">Register</a>
+            <a href="#download">Download Now</a>
+        </nav>
+    </div>
+</div>
+
+    <div class="homePage">
+        <div class="index">
+            <div class="intro">
+                <h3>De Gokkers</h3>
+                <h4>Summary</h4>
+                <span>Welcome by the Gokkers application.</span>
+                <p>This application is a game that simulates a bedding game.</p>
+            </div>
+
+            <div class="game">
+                <div class="app-info">
+
+                    <div class="gameDescription">
+                        <p>Description</p>
+                        <p>The game has three Player. The players are Sieste, Ferr and Lidy. And there are five zebra.</p>
+                        <p>you're goal is to pick one of the five zebra's that are available. pick you name mark the button and bet.</p>
+                        <p>When you win will gain the double form what you bet, but if you lose. You will lose the money that you bet.</p>
+                        <p>But watch out there. Weather is not so great in the Netherlands. it rains often!</p>
+                    </div>
+
+                    <div class="gameRules">
+
+                        <p>Rules</p>
+                            <ul>
+                            <li>Minimum amount to bet = € 5</li>
+                            <li>Maximum amount yo bet = € 15</li>
+                            <li>You can bet only one's</li>
+                            <li>Every player can only bet on one Zebra</li>
+                            <li>When a bet is placed there is no changing</li>
+                            <li>While the zebra are running the bets are closed of</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="register"><a name="register"></a>
+        <div class="register-center">
+            <h2>register</h2>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">E-mail adres: </label>
+                    <input type="email" name="email" id="email" required>
+                    <label for="password">Password: </label>
+                    <input type="password" name="password" id="password" required>
+                    <input type="submit" id="submit" value="Verstuur">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+</body>
+</html>
